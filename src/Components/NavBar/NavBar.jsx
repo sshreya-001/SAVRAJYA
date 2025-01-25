0ee2d1400c8ce268eb48f06408921e7ce71b4bb2
sshreya-001 <137506010+sshreya-001@users.noreply.github.com>
2025-01-25 21:33:25 +0530

    Commit

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import './NavBar.css';
-// import { Link } from 'react-router-dom'; 
-// const NavBar = () => {
-//   return (
-//     <div className="wrapper">
-//       <div className="left">
-//         <div className="name">SAVRAJYA</div>
-//       </div>
-//       <div className="right">
-//         <div className="list">
-//           <ul>
-//             <li><Link to="/intro">HOME</Link></li>
-//             <li><Link to="/knowus">ABOUT-US</Link></li>
-//             <li><Link to="/stories">STORIES</Link></li>
-//             <li><Link to="/reads">READ</Link></li>
-//             <li><Link to="/blogs">BLOGS</Link></li>
-//           </ul>
-//         </div>
-//         <Link to="/login">
-//           <button className="butt">LOGIN</button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NavBar;
-
-
 import React from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom'; 
